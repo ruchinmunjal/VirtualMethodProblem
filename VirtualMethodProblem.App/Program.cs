e68177d45d568fdf64d575602914cff07f157855
Ruchin Munjal <emailruchin@gmail.com>
2022-08-24 08:40:23 +0100

    The Problem

---
+++ VirtualMethodProblem.App/Program.cs
@@ -0,0 +1,35 @@
+﻿Base a = new Base(3 , new ());
+Derived d = new(4,new());
+
+Console.WriteLine($"base class state is {a.State} and calculate state is {a.Calculated}");
+Console.WriteLine($"derived class state is {d.State} and calculate state is {d.Calculated}");
+
+
+class Base
+{
+    public int State { get; }
+    public int Calculated { get; }
+    
+    public object Dependency { get; }
+
+    public Base(int state, object dependency)
+    {
+        this.Calculated = this.Calculate();
+        this.State = state;
+        Dependency = dependency;
+    }
+
+    protected virtual int Calculate() => 7;
+}
+
+class Derived : Base
+{
+    public Derived(int state, object dependency) : base(state,dependency)
+    {
+        
+    }
+    //Below will fail because Dependency is still null as the value used is before initialization
+    protected override int Calculate() => this.Dependency.GetHashCode() % base.State * 2;
+    
+
+}
